0ee33e001c938866ae1d24775e970a5fc823940f
zihanzhou <43834013+zihanzhou@users.noreply.github.com>
2020-11-13 20:08:44 -0500

    added check duplicate for closedlist

--- A_Star.py
+++ A_Star.py
@@ -10,9 +10,10 @@ import time
 
 """Global variables."""
 open_list = []
-closed_list = [] # new
+closed_list = []  # new
 solution_file_data = []
 search_file_data = []
+
 
 def update_solution_file_data(goal_node):
     """Fills solution_file_data list with data related to the ancestors of the goal node."""
@@ -20,33 +21,36 @@ def update_solution_file_data(goal_node):
     for i in goal_node.get_ancestors():
         solution_file_data.append((i.get_swapped_token(), i.get_swap_cost(),
                                    i.get_configuration()))
-    
+
+
 def update_search_file_data(current_node):
     """Fills search_file_data list with data related to the current node."""
     global search_file_data
-    
-    search_file_data.append((current_node.get_f(), current_node.get_g(), 
+
+    search_file_data.append((current_node.get_f(), current_node.get_g(),
                              current_node.get_h(), current_node.get_configuration()))
     # Different from Kevin's stuff
+
 
 def choose_heuristic(heuristic_number):
     """Applies correct heuristic method based on what was chosen."""
     if heuristic_number == 1:
         for i in open_list:
-            i.apply_heuristic_1() 
+            i.apply_heuristic_1()
             i.determine_f()
     elif heuristic_number == 2:
         for i in open_list:
-            i.apply_heuristic_2() 
+            i.apply_heuristic_2()
             i.determine_f()
     else:
         for i in open_list:
-            i.apply_heuristic_0() 
+            i.apply_heuristic_0()
             i.determine_f()
+
 
 def find_children_nodes(node, heuristic_number):
     """Appends children nodes to open list then sorts it accordingly."""
-    global open_list, closed_list # new
+    global open_list, closed_list  # new
     open_list.append(copy.deepcopy(node).move_left(copy.deepcopy(node)))
     open_list.append(copy.deepcopy(node).move_right(copy.deepcopy(node)))
     open_list.append(copy.deepcopy(node).move_down(copy.deepcopy(node)))
@@ -63,7 +67,7 @@ def find_children_nodes(node, heuristic_number):
     open_list.append(copy.deepcopy(node).wrap_diag_down_right(copy.deepcopy(node)))
     open_list.append(copy.deepcopy(node).wrap_diag_up_left(copy.deepcopy(node)))
     open_list.append(copy.deepcopy(node).wrap_diag_up_right(copy.deepcopy(node)))
-    
+
     # Remove None objects and then sort open list with lowest f first
     open_list = list(filter(None, open_list))
     choose_heuristic(heuristic_number)
@@ -72,31 +76,36 @@ def find_children_nodes(node, heuristic_number):
     temp_list = []
     temp_configuration = []
     for i in range(len(open_list)):
-        if (open_list[i].get_configuration() not in temp_configuration) and (open_list[i].get_configuration() not in closed_list):
+        if (open_list[i].get_configuration() not in temp_configuration) and (
+                open_list[i].get_configuration() not in closed_list):
             temp_configuration.append(open_list[i].get_configuration())
             temp_list.append(open_list[i])
     open_list = temp_list
     # new
 
+
 def apply_algorithm(start_node, heuristic_number):
     """Applies the A* algorithm given a start node."""
-    global open_list, closed_list # new
+    global open_list, closed_list  # new
     start_time = time.time()
     if heuristic_number == 1:
         start_node.apply_heuristic_1()
-        start_node.determine_f() # new
+        start_node.determine_f()  # new
     elif heuristic_number == 2:
         start_node.apply_heuristic_2()
-        start_node.determine_f() # new
+        start_node.determine_f()  # new
     else:
         start_node.apply_heuristic_0()
-        start_node.determine_f() # new
+        start_node.determine_f()  # new
     open_list.append(start_node)
     total_cost = 0
-    elapsed_time = 0 # new
-    while(open_list):
+    elapsed_time = 0  # new
+    while open_list:
         current_node = open_list.pop(0)
-        closed_list.append(current_node.get_configuration()) # new
+        configuration = current_node.get_configuration()
+        if configuration in closed_list:
+            continue
+        closed_list.append(configuration)  # new
         update_search_file_data(current_node)
         if current_node.is_goal():
             total_cost = current_node.get_g()
@@ -111,6 +120,6 @@ def apply_algorithm(start_node, heuristic_number):
         if elapsed_time > 60:
             return [], []
     solution_file_data.append((current_node.get_swapped_token(),
-                              current_node.get_swap_cost(), current_node.get_configuration()))
-    solution_file_data.append((total_cost, elapsed_time))    
+                               current_node.get_swap_cost(), current_node.get_configuration()))
+    solution_file_data.append((total_cost, elapsed_time))
     return solution_file_data, search_file_data
--- puzzle.py
+++ puzzle.py
@@ -5,9 +5,10 @@ Date:          November 16, 2020
 Description:   Code used for the creation of a Puzzle class.
 """
 
+
 class Puzzle():
     """A class to represent a puzzle node."""
-    
+
     def __init__(self, initial_state, goal_state_1, goal_state_2, row_length, column_length):
         """A constructor to initialize the attributes of a Puzzle object."""
         self.initial_state = initial_state
@@ -22,53 +23,53 @@ class Puzzle():
         self.f = 0
         self.swapped_token = 0
         self.swap_cost = 0
-    
+
     def get_g(self):
         """Returns the cost from root to current node."""
         return self.g
-    
+
     def get_h(self):
         """Returns the estimated lowest cost from node to goal node."""
         return self.h
-    
+
     def get_f(self):
         """Returns the combined cost of g and h."""
         return self.f
-    
+
     def get_configuration(self):
         """Returns the current configuration of the puzzle."""
         return self.current_state
-    
+
     def get_swapped_token(self):
         """Returns the swapped token."""
         return self.swapped_token
-    
+
     def get_swap_cost(self):
         """Returns the cost of the swap."""
         return self.swap_cost
-    
+
     def get_ancestors(self):
         """Returns list of ancestor puzzle objects for a node."""
         return self.ancestor_states
-    
+
     def determine_f(self):
         """Finds the combined cost of g and h."""
         self.f = self.g + self.h
-    
+
     def is_goal(self):
         """Determines if current node is the goal or not."""
         return (self.current_state == self.goal_state_1) or (self.current_state == self.goal_state_2)
-    
+
     def locate_empty_tile(self):
         """Determines the index of the empty tile in the puzzle."""
         return self.current_state.index(0)
-        
+
     def swap_tiles(self, empty_tile_index, tile_index):
         """Swap the index of the empty tile with another one."""
         self.swapped_token = self.current_state[tile_index]
         self.current_state[tile_index] = self.current_state[empty_tile_index]
         self.current_state[empty_tile_index] = self.swapped_token
-    
+
     def move_left(self, parent_node):
         """Regularly move the empty tile to the left."""
         empty_tile_index = self.locate_empty_tile()
@@ -82,12 +83,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def move_right(self, parent_node):
         """Regularly move the empty tile to the right."""
         empty_tile_index = self.locate_empty_tile()
         # If on right edge, can't move right
-        if empty_tile_index in range(self.row_length-1, len(self.current_state), self.row_length):
+        if empty_tile_index in range(self.row_length - 1, len(self.current_state), self.row_length):
             return
         distance = 1
         right_tile_index = empty_tile_index + distance
@@ -96,12 +97,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def move_down(self, parent_node):
         """Regularly move the empty tile down."""
         empty_tile_index = self.locate_empty_tile()
         # If on bottom edge, can't move down
-        if empty_tile_index in range(len(self.current_state)-self.row_length, len(self.current_state)):
+        if empty_tile_index in range(len(self.current_state) - self.row_length, len(self.current_state)):
             return
         distance = self.row_length
         lower_tile_index = empty_tile_index + distance
@@ -110,7 +111,7 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def move_up(self, parent_node):
         """Regularly move the empty tile up."""
         empty_tile_index = self.locate_empty_tile()
@@ -124,12 +125,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def wrap_left(self, parent_node):
         """Wrap the empty tile left to have it at the right end of the row."""
         empty_tile_index = self.locate_empty_tile()
         # If not in left corner, can't wrap left
-        if empty_tile_index not in [0, len(self.current_state)-self.row_length]:
+        if empty_tile_index not in [0, len(self.current_state) - self.row_length]:
             return
         distance = self.row_length - 1
         right_tile_index = empty_tile_index + distance
@@ -138,12 +139,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def wrap_right(self, parent_node):
         """Wrap the empty tile right to have it at the left end of the row."""
         empty_tile_index = self.locate_empty_tile()
         # If not in right corner, can't wrap right
-        if empty_tile_index not in [self.row_length-1, len(self.current_state)-1]:
+        if empty_tile_index not in [self.row_length - 1, len(self.current_state) - 1]:
             return
         distance = self.row_length - 1
         left_tile_index = empty_tile_index - distance
@@ -152,26 +153,26 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-    
+
     def wrap_down(self, parent_node):
         """Wrap the empty tile down to have it at the top end of the column."""
         empty_tile_index = self.locate_empty_tile()
         # If not in bottom corner, can't wrap down
-        if empty_tile_index not in [len(self.current_state)-self.row_length, len(self.current_state)-1]:
+        if empty_tile_index not in [len(self.current_state) - self.row_length, len(self.current_state) - 1]:
             return
         distance = self.row_length * (self.column_length - 1)
-        upper_tile_index = empty_tile_index - distance 
+        upper_tile_index = empty_tile_index - distance
         self.swap_tiles(empty_tile_index, upper_tile_index)
         self.swap_cost = 2
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def wrap_up(self, parent_node):
         """Wrap the empty tile up to have it at the bottom end of the column."""
         empty_tile_index = self.locate_empty_tile()
         # If not in top corner, can't wrap up
-        if empty_tile_index not in [0, self.row_length-1]:
+        if empty_tile_index not in [0, self.row_length - 1]:
             return
         distance = self.row_length * (self.column_length - 1)
         lower_tile_index = empty_tile_index + distance
@@ -180,12 +181,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-    
+
     def move_diag_down_left(self, parent_node):
         """Regularly move the empty tile diagonally down left."""
         empty_tile_index = self.locate_empty_tile()
         # If not in top right corner, can't move
-        if empty_tile_index != self.row_length-1:
+        if empty_tile_index != self.row_length - 1:
             return
         distance = self.row_length - 1
         lower_left_tile_index = empty_tile_index + distance
@@ -194,7 +195,7 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def move_diag_down_right(self, parent_node):
         """Regularly move the empty tile diagonally down right."""
         empty_tile_index = self.locate_empty_tile()
@@ -208,12 +209,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-    
+
     def move_diag_up_left(self, parent_node):
         """Regularly move the empty tile diagonally up left."""
         empty_tile_index = self.locate_empty_tile()
         # If not in bottom right corner, can't move
-        if empty_tile_index != len(self.current_state)-1:
+        if empty_tile_index != len(self.current_state) - 1:
             return
         distance = self.row_length + 1
         upper_left_tile_index = empty_tile_index - distance
@@ -222,12 +223,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def move_diag_up_right(self, parent_node):
         """Regularly move the empty tile diagonally up right."""
         empty_tile_index = self.locate_empty_tile()
         # If not in bottom left corner, can't move
-        if empty_tile_index != len(self.current_state)-self.row_length:
+        if empty_tile_index != len(self.current_state) - self.row_length:
             return
         distance = self.row_length - 1
         upper_right_tile_index = empty_tile_index - distance
@@ -236,12 +237,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def wrap_diag_down_left(self, parent_node):
         """Wrap the empty tile diagonally down left to have it in top right corner."""
         empty_tile_index = self.locate_empty_tile()
         # If not in bottom left corner, can't move
-        if empty_tile_index != len(self.current_state)-self.row_length:
+        if empty_tile_index != len(self.current_state) - self.row_length:
             return
         distance = self.row_length * (self.column_length - 2) + 1
         upper_right_tile_index = empty_tile_index - distance
@@ -250,12 +251,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def wrap_diag_down_right(self, parent_node):
         """Wrap the empty tile diagonally down right to have it in top left corner."""
         empty_tile_index = self.locate_empty_tile()
         # If not in bottom right corner, can't move
-        if empty_tile_index != len(self.current_state)-1:
+        if empty_tile_index != len(self.current_state) - 1:
             return
         distance = self.row_length * self.column_length - 1
         upper_left_tile_index = empty_tile_index - distance
@@ -264,7 +265,7 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-    
+
     def wrap_diag_up_left(self, parent_node):
         """Wrap the empty tile diagonally up left to have it in bottom right corner."""
         empty_tile_index = self.locate_empty_tile()
@@ -278,12 +279,12 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-        
+
     def wrap_diag_up_right(self, parent_node):
         """Wrap the empty tile diagonally up right to have it in bottom left corner."""
         empty_tile_index = self.locate_empty_tile()
         # If not in top right corner, can't move
-        if empty_tile_index != self.row_length-1:
+        if empty_tile_index != self.row_length - 1:
             return
         distance = self.row_length * (self.column_length - 2) + 1
         lower_left_tile_index = empty_tile_index + distance
@@ -292,14 +293,14 @@ class Puzzle():
         self.g += self.swap_cost
         self.ancestor_states.append(parent_node)
         return self
-    
+
     def apply_heuristic_0(self):
         """Applies a naive heuristic."""
-        if (self.current_state[-1] == 0):
+        if self.current_state[-1] == 0:
             self.h = 0
         else:
             self.h = 1
-        
+
     def apply_heuristic_1(self):
         """Applies a heuristic based on Hamming distance to count number of 
         tiles out of place."""
@@ -314,26 +315,26 @@ class Puzzle():
                 if self.current_state[i] != self.goal_state_2[i]:
                     temp_2 += 1
         self.h = min(temp_1, temp_2)
-        
+
     # Dont think it always gives expected result but could be wrong!
     def apply_heuristic_2(self):
         """Applies a heuristic based on Manhattan distance to sum up 
         all the distances by which tiles are out of place."""
         temp_1 = 0
         temp_2 = 0
-        temp_1 = sum(abs(a%self.row_length - b%self.row_length)
-                     + abs(a//self.column_length - b//self.column_length)
-                         for a, b in ((self.current_state.index(i), self.goal_state_1.index(i))
-                             for i in range(1, len(self.current_state))))
-        temp_2 = sum(abs(a%self.row_length - b%self.row_length)
-                     + abs(a//self.column_length - b//self.column_length)
-                         for a, b in ((self.current_state.index(i), self.goal_state_2.index(i))
-                             for i in range(1, len(self.current_state))))
+        temp_1 = sum(abs(a % self.row_length - b % self.row_length)
+                     + abs(a // self.column_length - b // self.column_length)
+                     for a, b in ((self.current_state.index(i), self.goal_state_1.index(i))
+                                  for i in range(1, len(self.current_state))))
+        temp_2 = sum(abs(a % self.row_length - b % self.row_length)
+                     + abs(a // self.column_length - b // self.column_length)
+                     for a, b in ((self.current_state.index(i), self.goal_state_2.index(i))
+                                  for i in range(1, len(self.current_state))))
         # print("h2 => g1=>", temp_1)
         # print("h2 => g2=>", temp_2)
         self.h = min(temp_1, temp_2)
 
-#-----------------------------------------------
+# -----------------------------------------------
 # puz = Puzzle([2, 0, 3, 4, 1, 5, 6, 7], 4, 2)     
 # print(puz.current_state)
 # # print(puz.h)
